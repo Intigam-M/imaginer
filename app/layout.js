fdd0ea72e10fe26fc4252a512d71d33df276c65e
Intigam Mammadli <intigam.mammadli@gmail.com>
2023-07-12 09:51:16 +0400

    Create Global, reset CSS

--- app/layout.js
+++ app/layout.js
@@ -1,11 +1,14 @@
-import { Inter } from 'next/font/google'
-
-const inter = Inter({ subsets: ['latin'] })
+import "@/styles/reset.css"
+import "@/styles/variables.css"
+import "@/styles/global.css"
 
 export default function RootLayout({ children }) {
-  return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
-  )
+    return (
+        <html lang="en">
+            <body className="layout">
+                {children}
+                <div className="overlay"></div>
+            </body>
+        </html>
+    )
 }
